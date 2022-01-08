bfe374b40128025386e9c3a0d2f5b184b831d624
blue-emc2 <s.yamazaki@everyleaf.com>
2022-01-08 17:26:19 +0900

    コマンドの引数に検索パターンを追加

--- lib/ludolph/cli.ex
+++ lib/ludolph/cli.ex
@@ -1,21 +1,41 @@
 defmodule Ludolph.CLI do
-  def run(argv) do
-    parse_args(argv)
+  def main(argv) do
+    argv
+    |> parse_args()
+    |> process()
   end
 
   def parse_args(argv) do
     parse =
       OptionParser.parse(argv,
-        aliases: [s: :single, m: :multi, h: :help],
-        strict: [single: :boolean, multi: :boolean, help: :boolean]
+        aliases: [s: :single, m: :multi, h: :help, p: :pattern],
+        strict: [single: :boolean, multi: :boolean, help: :boolean, pattern: :string]
       )
 
     case parse do
       {[_, help: true], _path, _} -> :help
-      {[single: true, multi: true], _path, _} -> :help
-      {[single: true], path, _} -> {:single, List.first(path)}
-      {[multi: true], path, _} -> {:multi, List.first(path)}
+      {[single: true, multi: true, pattern: _], _path, _} -> :help
+      {[single: true, pattern: pattern], path, _} -> {:single, pattern, List.first(path)}
+      {[multi: true, pattern: pattern], path, _} -> {:multi, pattern, List.first(path)}
       _ -> :help
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: ludolph [-s | -m] path/to/pi.txt
+    """
+
+    System.halt(0)
+  end
+
+  def process({:single, _path}) do
+    IO.puts("single")
+    System.halt(0)
+  end
+
+  def process({:multi, _path}) do
+    IO.puts("multi")
+    System.halt(0)
   end
 end
--- test/ludolph_test.exs
+++ test/ludolph_test.exs
@@ -4,20 +4,28 @@ defmodule LudolphTest do
 
   import Ludolph.CLI, only: [parse_args: 1]
 
-  test "-hか--helpを指定したら:helpが取得できる" do
+  test "-hか--helpを指定したらヘルプが起動する" do
     assert parse_args(["-s", "-h"]) == :help
     assert parse_args(["-s", "--help"]) == :help
   end
 
-  test "-sとfilepathを指定したらキーワードリストが取得できる" do
-    assert parse_args(["-s", "/home/workspace/pi.txt"]) == {:single, "/home/workspace/pi.txt"}
+  test "シングルモードで起動する" do
+    assert parse_args(["-s", "/home/workspace/pi.txt", "-p", "1234"]) == {:single, "1234", "/home/workspace/pi.txt"}
   end
 
-  test "-mとfilepathを指定したらキーワードリストが取得できる" do
-    assert parse_args(["-m", "/home/workspace/pi.txt"]) == {:multi, "/home/workspace/pi.txt"}
+  test "マルチモードで起動する" do
+    assert parse_args(["-m", "/home/workspace/pi.txt", "-p", "1234"]) == {:multi, "1234", "/home/workspace/pi.txt"}
   end
 
-  test "-sと-mを両方指定したら:helpが取得できる" do
+  test "シングルとマルチモードは共存できないのでヘルプが起動する" do
     assert parse_args(["-m", "-s", "/home/workspace/pi.txt"]) == :help
+  end
+
+  test "検索パターンが与えられなかったらヘルプが起動する" do
+    assert parse_args(["-m", "/home/workspace/pi.txt", "1234"]) == :help
+  end
+
+  test "ファイルパスが与えられなかったらヘルプが起動する" do
+    assert parse_args(["-s", "1234"]) == :help
   end
 end
